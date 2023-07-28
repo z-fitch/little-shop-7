55314d4fbdf579345d0bf879ecc766c5440d5ef2
Jorja Fleming <aset284@gmail.com>
2023-07-28 15:46:20 -0600

    Refactor to update tests

--- spec/features/admin/admin_merchant_spec.rb
+++ spec/features/admin/admin_merchant_spec.rb
@@ -3,11 +3,15 @@ require "rails_helper"
 RSpec.describe "Admin Merchant Page", type: :feature do
   describe "When I visit the merchant index (/admin/merchants)" do
     # US 24
-    it "I a list of all the merchants" do
-      merchant_1 = Merchant.create!(name: "Merchant 1")
+    it "I see a list of all the merchants" do
       visit admin_merchants_path
+      expect(Merchant.all.count).to eq(3) # sanity check
 
-      expect(page).to have_content("Merchant 1")
+      Merchant.all.each do |merchant|
+        expect(page).to have_content(merchant.name)
+      end
+      
+      expect(page).not_to have_link("The Android's Dungeon & Baseball Card Shop")
     end
   end
 end
